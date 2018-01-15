1aca12a29c7929416f17e4b54a34666bdfb3a451
Erik E. Lorenz <erik@tuvero.de>
2018-01-15 20:29:07 +0100

    Initial Commit

---
+++ src/actions.ts
@@ -0,0 +1,7 @@
+import * as state from './state/actions';
+
+let actions = {
+  state: state.actions
+};
+
+export { actions };
---
+++ src/state/actions.ts
@@ -0,0 +1,17 @@
+﻿interface State {
+  name: string;
+  target: string;
+  version: string;
+}
+
+let actions = {
+  create(name: string, target: string) : State {
+    return {
+      name: name,
+      target: target,
+      version: '1.5.8'
+    };
+  }
+};
+
+export { actions };
