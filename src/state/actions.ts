8aed69e09fa25c165fcd9c95dde9a0094237567f
Erik E. Lorenz <erik@tuvero.de>
2018-02-17 18:17:35 +0100

    autofix in actions.ts: let->const, spacing, quotes

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,15 +1,15 @@
-﻿interface State {
+interface State {
   name: string;
   target: string;
   version: string;
 }
 
-let actions = {
-  create(name: string, target: string) : State {
+const actions = {
+  create(name: string, target: string): State {
     return {
       name: name,
       target: target,
-      version: '1.5.8'
+      version: "1.5.8"
     };
   }
 };
