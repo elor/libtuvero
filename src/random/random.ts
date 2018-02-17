711d401705353a00cd9bdaf8f1f551ca0bc1f4f3
Erik E. Lorenz <erik@tuvero.de>
2018-02-17 09:17:13 +0100

    Add random.int

--- src/random/random.ts
+++ src/random/random.ts
@@ -1,7 +1,18 @@
 ﻿import { rand } from "mersenne";
 
+function int(min: number, max: number = 0): number {
+  if (max < min) {
+    return int(max, min);
+  }
+
+  return min + rand(max - min);
+}
+
 function pick(array: number[]): number {
   return array[rand(array.length)];
 }
 
-export { pick };
+export {
+  int,
+  pick,
+};
