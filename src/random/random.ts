48657987dde752a3d6e88736e74a5dc24b0fad04
Erik E. Lorenz <erik@tuvero.de>
2018-02-17 09:28:14 +0100

    Add random.shuffle

--- src/random/random.ts
+++ src/random/random.ts
@@ -1,4 +1,5 @@
 ﻿import { rand } from "mersenne";
+import { range } from "lodash";
 
 function int(min: number, max: number = 0): number {
   if (max < min) {
@@ -16,8 +17,20 @@ function pluck(array: number[]): number {
   return array.splice(rand(array.length), 1)[0];
 }
 
+function shuffle(array: number[]): number[] {
+  let indices = range(array.length);
+  let randomIndices = [];
+
+  while (indices.length) {
+    randomIndices.push(pluck(indices));
+  }
+
+  return randomIndices.map(i => array[i]);
+}
+
 export {
   int,
   pick,
   pluck,
+  shuffle
 };
