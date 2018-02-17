e51b0c0904bce91959e38d35d76320bd1001b9fa
Erik E. Lorenz <erik@tuvero.de>
2018-02-17 18:18:29 +0100

    Add range check for random.int()

--- src/random/random.spec.ts
+++ src/random/random.spec.ts
@@ -1,4 +1,5 @@
 import * as chai from "chai";
+import { range } from "lodash";
 
 import random from "./random";
 
@@ -6,6 +7,10 @@ describe("random/random.ts", () => {
   describe("int()", () => {
     it("int(0) equals 0", () => {
       chai.assert.equal(random.int(0), 0);
+    });
+
+    it("stays in range", () => {
+      range(1, 1000).forEach(max => chai.assert.include(range(max), random.int(max), `max=${max}`));
     });
   });
 });
