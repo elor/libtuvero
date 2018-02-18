2af20c5d18cfcde81c8bf6fad1bae6dac73800f4
Erik E. Lorenz <erik@tuvero.de>
2018-02-18 09:37:49 +0100

    Load webpack config from package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+const package = require("./package.json");
 
 module.exports = {
-  entry: './dist/index.js',
+  entry: package.main,
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'var', // var, umd, commonjs2, commonjs, amd, this, assign, window, global, jsonp
-    library: 'tuvero'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var", // var, umd, commonjs2, commonjs, amd, this, assign, window, global, jsonp
+    library: package.name
   },
   plugins: [
     new UglifyJSPlugin()
