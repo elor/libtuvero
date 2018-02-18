09a698585a3dfecf16c1217e9f49e1dee6bb05c9
Erik E. Lorenz <erik@tuvero.de>
2018-02-18 11:04:31 +0100

    Test random.ts for min/max swapping

--- src/random/random.spec.ts
+++ src/random/random.spec.ts
@@ -12,5 +12,12 @@ describe("random/random.ts", () => {
     it("stays in range", () => {
       range(1, 1000).forEach(max => chai.assert.include(range(max), random.int(max), `max=${max}`));
     });
+
+    it("swaps max and min if necessary", () => {
+      const min = 10;
+      const max = 0;
+      range(100).forEach(() => chai.assert.isBelow(random.int(min, max), min));
+      range(100).forEach(() => chai.assert.isAtLeast(random.int(min, max), max));
+    });
   });
 });
