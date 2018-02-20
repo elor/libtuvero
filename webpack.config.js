f3c7c7e6ced3653206a0414027a72f5efe364d46
Erik E. Lorenz <erik@tuvero.de>
2018-02-20 22:12:38 +0100

    Build multiple webpack targets: AMD, CommonJS, ...

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,26 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const package = require("./package.json");
 
-module.exports = {
+/**
+ * @param target var, umd, commonjs2, commonjs, amd, this, assign, window, global or jsonp
+ */
+const makeConfig = target => ({
   entry: package.main,
   output: {
-    filename: "bundle.js",
+    filename: `tuvero.bundle-${target}.js`,
     path: path.resolve(__dirname, "dist"),
-    libraryTarget: "var", // var, umd, commonjs2, commonjs, amd, this, assign, window, global, jsonp
+    libraryTarget: target,
     library: package.name
   },
   plugins: [
     new UglifyJSPlugin()
   ]
-};
+});
+
+module.exports = [
+  makeConfig("amd"),
+  makeConfig("var"),
+  makeConfig("commonjs2"),
+  makeConfig("commonjs"),
+  makeConfig("window")
+]
