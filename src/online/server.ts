ff1c9c1ee26697895a72c106d6cd0a66457fe60c
Erik E. Lorenz <erik@tuvero.de>
2018-03-07 21:18:47 +0100

    Add options to server.ts (e.g. for unit testing)
     - Options currently are config overrides

--- src/online/server.ts
+++ src/online/server.ts
@@ -1,16 +1,38 @@
 ﻿import axios from "axios";
 import config from "../config";
+import * as _ from "lodash";
 
-const server = {
+type API_Config = {
+  baseUrl: string,
+  defaultPath: string,
+  timeout: number
+}
+
+type API_Options = {
+  baseUrl?: string,
+  defaultPath?: string,
+  timeout?: number
+}
+
+class Server {
+  token: string;
+  config: API_Config;
+
+  constructor(token: string = "", options?: API_Options) {
+    this.token = token;
+
+    this.config = _.extend({}, config.online.api, options);
+  }
+
   /**
    * Retrieve an object from the tuvero cloud
    *
    * @param path The path. Defaults to "/".
    * @param timeout Optional. Timeout in milliseconds.
    */
-  get(path: string = "/", timeout: number = config.online.api.timeout): Promise<Object> {
-    return axios.get(`${config.online.api.baseUrl}/${path}`, { timeout });
-  },
+  get(path: string = "/", timeout: number = this.config.timeout): Promise<Object> {
+    return axios.get(`${this.config.baseUrl}/${path}`, { timeout });
+  }
 
   /**
    * Update an object in the Tuvero Cloud
@@ -19,9 +41,9 @@ const server = {
    * @param data A data object to post to the server.
    * @param timeout Optional.
    */
-  post(path: string, data: Object, timeout: number = config.online.api.timeout): Promise<Object> {
-    return axios.post(`${config.online.api.baseUrl}/${path}`, data, { timeout });
-  },
+  post(path: string, data: Object, timeout: number = this.config.timeout): Promise<Object> {
+    return axios.post(`${this.config.baseUrl}/${path}`, data, { timeout });
+  }
 
   /**
    * Create an Object in the Tuvero Cloud
@@ -30,9 +52,9 @@ const server = {
    * @param data The Object to create on the server
    * @param timeout Optional. Timeout in milliseconds.
    */
-  put(path: string, data: Object, timeout: number = config.online.api.timeout): Promise<Object> {
-    return axios.put(`${config.online.api.baseUrl}/${path}`, data, { timeout });
-  },
+  put(path: string, data: Object, timeout: number = this.config.timeout): Promise<Object> {
+    return axios.put(`${this.config.baseUrl}/${path}`, data, { timeout });
+  }
 
   /**
    * Delete an Object from the Tuvero Cloud
@@ -40,9 +62,9 @@ const server = {
    * @param path The path
    * @param timeout Optional. Timeout in milliseconds.
    */
-  delete(path: string = "/", timeout: number = config.online.api.timeout): Promise<Object> {
-    return axios.delete(`${config.online.api.baseUrl}/${path}`, { timeout });
+  delete(path: string = "/", timeout: number = this.config.timeout): Promise<Object> {
+    return axios.delete(`${this.config.baseUrl}/${path}`, { timeout });
   }
 };
 
-export default server;
+export default Server;
